73407baef49ccc2ea3a0c9062eda314d3afa3757
Darcy140604 <darcyfranklin14@gmail.com>
2025-10-05 11:12:30 +0300

    feat: 10- Mise en place du systeme 'alert (v1)

---
+++ src/Composants/Alert/Alert.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Alert.css';
+
+export function Alert() {
+  return (
+    <div className='container_alert bg-green-300 w-130 h-25 rounded-3xl shadow-lg'>
+      
+    </div>
+  )
+}
